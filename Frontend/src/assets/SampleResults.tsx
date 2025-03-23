d80ff44caf8c975fc1ae46e2eed4d7f7596255d3
kratipandya <71118164+kratipandya@users.noreply.github.com>
2025-03-23 10:37:00 +0200

    Add files via upload

---
+++ Frontend/src/assets/SampleResults.tsx
@@ -0,0 +1,35 @@
+export const SampleResults = [
+    {
+      id: "arXiv:2503.10550",
+      title: "Fast-response low power atomic oven for integration into an ion microchip",
+      authors: ["Vijay Kumar", "Martin Siegele-Brown", "Parsa Rahimi", "Matthew Aylett", "Sebastian Weidt", "Winfried Karl Hensinger"],
+      abstract: "We present a novel microfabricated neutral atom source for quantum technologies that can be easily integrated onto microchip devices using well-established MEMS fabrication techniques, and contrast this to conventional off-chip ion loading mechanisms. The heating filament of the device is shown to be as small as 90×90 μm².",
+      categories: ["physics.app-ph", "quant-ph"],
+      contentTypes: ["pdf", "images", "equations"],
+      submissionDate: "13 March, 2025",
+      figures: 5,
+      pages: 5
+    },
+    {
+      id: "arXiv:2503.10492",
+      title: "Meta-learning characteristics and dynamics of quantum systems",
+      authors: ["Lucas Schorling", "Pranav Vaidhyanathan", "Jonas Schuff", "Miguel J. Carballido", "Dominik Zumbühl", "Gerard Milburn", "Florian Marquardt", "Jakob Foerster", "Michael A. Osborne", "Natalia Ares"],
+      abstract: "While machine learning holds great promise for quantum technologies, most current methods focus on predicting or controlling a specific quantum system. Meta-learning approaches, however, can adapt to new systems for which little data is available, by leveraging knowledge obtained from previous data associated with similar systems.",
+      categories: ["quant-ph", "cond-mat.mes-hall", "cs.LG", "physics.comp-ph"],
+      contentTypes: ["pdf", "images", "code", "equations"],
+      submissionDate: "13 March, 2025",
+      figures: 4,
+      pages: 6
+    },
+    {
+      id: "arXiv:2503.10463",
+      title: "Solving Dicke superradiance analytically: A compendium of methods",
+      authors: ["Raphael Holzinger", "Nico S. Bassler", "Julian Lyne", "Fidel G. Jimenez", "Julius T. Gohsrich", "Claudiu Genes"],
+      abstract: "Identical two-level systems (spins), undergoing collective spontaneous emission. This is one of the simplest examples of an open quantum system dynamics problem that admits a fully analytical solution. We provide a few different approaches to derive the analytical solution valid for any time during the evolution and for any number of spins.",
+      categories: ["quant-ph", "physics.optics"],
+      contentTypes: ["pdf", "images", "equations"],
+      submissionDate: "13 March, 2025",
+      figures: 2,
+      pages: 9
+    }
+  ];
